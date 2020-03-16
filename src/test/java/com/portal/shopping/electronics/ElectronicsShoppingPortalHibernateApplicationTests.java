13c8195cbfd721090e3aaf69abe098b52bef1ab7
Dasu <s.mariyadasu@gmail.com>
2020-03-16 14:45:54 +0530

    changed the path

---
+++ src/test/java/com/portal/shopping/electronics/ElectronicsShoppingPortalHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portal.shopping.electronics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectronicsShoppingPortalHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
